6d7f41f91306f557b4fa35d450dc8eb09e17106c
Frank Silva <101152046+FrankSilva01@users.noreply.github.com>
2022-08-18 22:45:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
-    let endereco = document.getElementById('valorCep').value;
-    
+ let endereco = document.getElementById('valorCep').value;
+    var btn = document.querySelector('#pesquisar');
+
+
+
     function buscarCep(){
         const URL = 'https://viacep.com.br/ws/'+`${endereco}`+'/json/'
         fetch(`${URL}`)
@@ -8,6 +11,10 @@
       .then((data) => {
         document.getElementById('resultado').innerHTML =  data.logradouro  + " CEP: "+ data.cep + " " + " Bairro: " + data.bairro + " Cidade: " + data.localidade + " " + "Estado: " + data.uf;
         console.log(data);
+       
+        btn.addEventListener('click', function () {
+          location.reload();
+        });
       })
       .catch((error) => console.error('Erro:', error.message || error))
     }
